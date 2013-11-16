93851f58cefa6eac7a291eac4abfa00dc9f50d97
Jejurikar <jezziejezz@yahoo.com>
2013-11-16 15:02:46 -0800

    Actually the soln looks simple, just to retun a ramdom sample

--- randomShuffle.pl
+++ randomShuffle.pl
@@ -2,46 +2,32 @@
 
 # @Copyright 2013: 
 #   The main requirements of the program are as follows:
-# - We have a DB and the top 500 users (we store the top500 in a csv (or txt) file)
-# - For each user, display k (e.g. 10) of the top500 users
+# - We have a DB and the top 500 users stored separately (we store the top500 in a csv (or txt) file)
+# - For each user, display k (e.g. 10) of the top500 users (or maybe a subset ot the top500)
 # - different users can (should) have different order of k top500 users
 # - Each user has a window (cookie and time based) where the order does not change
 #
 # Solution Description:
+#    This is a client server model, where the cookies can be maintained at the client side.
+#    This cookie can be used on the client side to ensure that the top user list does not 
+#    change on the client machine. In short the client will not request the server for a new
+#    list based on the cookie (timestamps etc.). This frees the server from tracking users and
+#    the (random) top user list for each user. So for every request the server returns a new 
+#    random list of top users (say k top users)
+#
 # 1. We have a big DB and we can run a query to get the list of top500 users. This is stored 
 #     may be in a separate DB - and it can be periodically updated if the top500 changes. If the 
 #     top500 is static, then this top500 list need not be updated.
-# 2. From the top500 list, we can create multiple (n) lists of smaller size. E.g. we create 5 list.
-#    The list can be of any size, depending on how many users are typically displayed. Or even how
-#    to want to utilize the list to have different orderings for different users. E.g we have lists
-#    as L0, L1, L2, L3, L4 (if we have 5 lists) 
-#    API to create the list: CreateList(offset, viewSize, lisSize);
-# 3. Each user has a cookie for his activity. The cookie stores the info on the list to be used. 
-#    The cookie keeps track of time of the accesses to the website.  
-#    The list ID is passed along with the request for the k users in top500.
+# 2.  Multiple lists are not required (server sends a new random list everytime)
 #
-# 4. List Management at server: Analogously, there can be some timers and respective list management
-#    at the server to know which list are no longer active (no user needs the list). 
-#    There can be some 'garbage collection' equivalent scheme to know the active lists and
-#    maybe refresh lists (using API in item 2) to recreate newer lists.   
+# 3. Each user has a cookie for his activity. The cookie stores the time of previous request 
+#    for the top user list from the server. This is used to request or not request a new list 
+#    from the server (as part of accessing the web-page i.e. viewing the top users list)
 #
-#    When a user specifies a list, the operations can be one of many options
-#    (a) Simple option: Start from i=0 and return the k items from the list (0 ... k-1). 
-#        Note that this limts the sharing of the lists among users (as different users want 
-#        to have different orderings),
-#    (b) Number theory based different list traversing : 
-#        In addition to the list, we can have a function to get the items from 
-#        the given list. E.g. we have lists of size=prime_number. This can enable some 
-#        'number theory' techniques (using modulo arithmetic) to efficiently generate different 
-#        orderings for the differnt users. We can store a 'generators' of the field and that 
-#        will give a different ordering for each different ordering.
-#    (c) Another Option: On top of that we can maybe have simple techniques like adding constants, 
-#        to this ordering to generate different ordering. Basically, we can reduce the number of 
-#        lists needed as the number of users change.
+# 4. List Management at server: Not required. Everytime, return a new random list.
 #
 # Coding: So from a coding perspective, we implement simple functions
-# - create a list
-# - shuffle a list  
+# - return a random sample of the top500 users (or a sub-set of that)
 
 use strict;
 use Data::Dumper;
@@ -67,7 +53,7 @@ sub readTop500File($$$)
 	return \@inputList;
 #print Dumper(\@inputList);
 }
-sub shuffleArray($)
+sub computeRandomSample($)
 {
 	my ($aref_array) = @_;
 # TODO: write code;
@@ -82,7 +68,12 @@ sub main()
 
 }
 
-main();
+#main();
 
-
+foreach my $i (1..10)
+{
+# int(rand(n)) retuns a number between 0..n-1 i.e. (0<= r <= n-1)
+	my $r = int(rand(10));
+	print(" i=$i $r \n");
+}
 
